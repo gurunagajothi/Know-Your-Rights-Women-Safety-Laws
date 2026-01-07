f72b1a47424e5dc579cc6ed41ceb2effdbdd44d8
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-07 22:41:35 +0530

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,10 +1,8 @@
 function reply(){
- let q = document.getElementById("userInput").value.toLowerCase();
- let r = "Please call 181 or 112";
-
- if(q.includes("law")) r="IPC 354 protects women";
- if(q.includes("help")) r="Press SOS or call police";
- if(q.includes("abuse")) r="Domestic Violence Act applies";
-
- document.getElementById("botReply").innerText = r;
+  const q = document.getElementById("userInput").value.toLowerCase();
+  let r="Please call 181 or 112";
+  if(q.includes("law")) r="IPC 354 protects women";
+  if(q.includes("help")) r="Press SOS or call police";
+  if(q.includes("abuse")) r="Domestic Violence Act applies";
+  document.getElementById("botReply").innerText=r;
 }
