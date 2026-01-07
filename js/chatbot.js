b917e8616b33e8cca4aef0cf5581ef0f43b59ca1
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-07 22:30:33 +0530

    Create chatbot.js

---
+++ js/chatbot.js
@@ -0,0 +1,10 @@
+function reply(){
+ let q = document.getElementById("userInput").value.toLowerCase();
+ let r = "Please call 181 or 112";
+
+ if(q.includes("law")) r="IPC 354 protects women";
+ if(q.includes("help")) r="Press SOS or call police";
+ if(q.includes("abuse")) r="Domestic Violence Act applies";
+
+ document.getElementById("botReply").innerText = r;
+}
