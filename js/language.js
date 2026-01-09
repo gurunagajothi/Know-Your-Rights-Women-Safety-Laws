8b85ecaa5888a5a31cba069d2516ed9975940c01
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-09 15:01:38 +0530

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,15 +1,6 @@
-function changeLanguage(lang){
-  if(lang==="ta"){
-    title.innerText="பெண்கள் பாதுகாப்பு சட்டங்கள்";
-    rule1.innerText="எப்போதும் எச்சரிக்கையாக இருங்கள்";
-    rule2.innerText="உங்கள் இருப்பிடத்தை பகிருங்கள்";
-  } else if(lang==="hi"){
-    title.innerText="महिला सुरक्षा नियम";
-    rule1.innerText="हमेशा सतर्क रहें";
-    rule2.innerText="अपना स्थान साझा करें";
-  } else {
-    title.innerText="Women Safety Rules";
-    rule1.innerText="Always stay alert";
-    rule2.innerText="Share your location";
-  }
-}
+const lang = {
+ta: { title: "பெண்கள் பாதுகாப்பு சட்டங்கள்" },
+hi: { title: "महिला सुरक्षा कानून" },
+en: { title: "Women Safety Laws" }
+};
+function setLang(l){ document.getElementById('title').innerText = lang[l].title }
