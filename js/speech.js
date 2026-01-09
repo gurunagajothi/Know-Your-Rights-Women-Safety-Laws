b4cfbab9463d601e5496a82adad6e883267a28cf
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-09 21:23:28 +0530

    Update speech.js

--- js/speech.js
+++ js/speech.js
@@ -1,6 +1,6 @@
 let synth = window.speechSynthesis;
 let utterance;
-let currentLang = "en-IN";
+let currentLang = "en-IN"; // default English
 
 function setLanguage(lang) {
   if (lang === "en") currentLang = "en-IN";
@@ -10,12 +10,19 @@ function setLanguage(lang) {
 
 function startReading() {
   stopReading();
+
   const text = document.getElementById("lawsContent").innerText;
+
   utterance = new SpeechSynthesisUtterance(text);
   utterance.lang = currentLang;
+  utterance.rate = 1;
+  utterance.pitch = 1;
+
   synth.speak(utterance);
 }
 
 function stopReading() {
-  if (synth.speaking) synth.cancel();
+  if (synth.speaking) {
+    synth.cancel();
+  }
 }
